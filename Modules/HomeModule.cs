6fad651207ccf23840393eb71959b409d7cb0dc2
Wingo <cmwingo@gmail.com>
2016-12-09 13:26:50 -0800

    Start up the server and return the index View

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace HairSalon
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["index.cshtml", AllStylists];
+      };
+    }
+  }
+}
