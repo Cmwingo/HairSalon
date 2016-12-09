a0c779331be860f23ff17f96a8a0b6f4fc43f18e
Wingo <cmwingo@gmail.com>
2016-12-09 14:03:30 -0800

    UI now adds clients

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -28,6 +28,22 @@ namespace HairSalon
         newStylist.Save();
         return View["success.cshtml"];
       };
+
+      Get["/clients"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["clients.cshtml", AllClients];
+      };
+
+      Get["/clients/new"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["clients_form.cshtml", AllStylists];
+      };
+
+      Post["/clients/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"], Request.Form["client-appointment-day"], Request.Form["client-appointment-time"]);
+        newClient.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
