0f884ea5e887a2dd04bb67a670a369c9e0370c35
Wingo <cmwingo@gmail.com>
2016-12-09 13:42:07 -0800

    UI can add a stylist

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,6 +13,21 @@ namespace HairSalon
         List<Stylist> AllStylists = Stylist.GetAll();
         return View["index.cshtml", AllStylists];
       };
+
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Get["/stylists/new"] = _ => {
+        return View["stylists_form.cshtml"];
+      };
+
+      Post["/stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-availability"], Request.Form["stylist-services"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
